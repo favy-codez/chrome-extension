af9e82a4d0d9ca8b95c7206239c18dbb27db32d9
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-12-23 10:33:11 +0100

    commit message

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ let inputBtn = document.getElementById("input-btn")
 
 // we spectify the event we want to listen to (click) and the fxn we want to evoke
 inputBtn.addEventListener("click", function(){
+    // ************
+// ** challenge **
+    // Push the value from the inputEl into the myLeads array 
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript" - we use .value for this
+// ************
+
     myLeads.push(inputEl.value)
     console.log("myLeads")
 })
@@ -12,10 +19,5 @@ inputBtn.addEventListener("click", function(){
 //     console.log("Button clicked!")
 //  }
 
-// ************
-// ** challenge **
-    // Push the value from the inputEl into the myLeads array 
-    // instead of the hard-coded "www.awesomeleads.com" value
-    // Google -> "get value from input field javascript" - we use .value for this
-// ************
+
 
