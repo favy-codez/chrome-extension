67a370b965b4074b89737d5c7b3027e801996f81
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-12-23 09:03:20 +0100

    commit message

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function saveLead(){
+let inputBtn = document.getElementById("input-btn")
+
+// we spectify the event we want to listen to (click) and the fxn we want to evoke
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from addEventListener")
     
-}
+})
